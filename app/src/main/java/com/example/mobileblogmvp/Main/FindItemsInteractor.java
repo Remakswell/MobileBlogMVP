60264d1d1895d48bb3a3a989647b6a6cccba1703
maksym <maksymshymko@gmail.com>
2019-03-13 21:55:12 +0200

    Getting projects and displaying a list on the screen. MVP

--- app/src/main/java/com/example/mobileblogmvp/Main/FindItemsInteractor.java
+++ app/src/main/java/com/example/mobileblogmvp/Main/FindItemsInteractor.java
@@ -1,16 +1,46 @@
 package com.example.mobileblogmvp.Main;
 
-import com.example.mobileblogmvp.Models.ProjectsResponse;
+import android.util.Log;
 
+import com.example.mobileblogmvp.ApiClient;
+import com.example.mobileblogmvp.ApiInterface;
+import com.example.mobileblogmvp.Models.ProjectsResponse;
 import java.util.List;
 
-import retrofit2.Retrofit;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class FindItemsInteractor {
 
-    interface OnFinishedListener {
+    private List<ProjectsResponse> projects;
+    private MainView mainView;
+    private final ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
 
+    public FindItemsInteractor(MainView mainView){
+        this.mainView = mainView;
     }
 
+    public void createProjectList() {
+        Call<List<ProjectsResponse>> getProjectsCall = apiInterface.
+                getProjects("en", "Bearer " + mainView.getToken());
 
+        getProjectsCall.enqueue(new Callback<List<ProjectsResponse>>() {
+            @Override
+            public void onResponse(Call<List<ProjectsResponse>> call, Response<List<ProjectsResponse>> response) {
+
+                projects = response.body();
+
+                if (mainView != null){
+                    mainView.setItems(projects);
+                    mainView.hideProgress();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<ProjectsResponse>> call, Throwable t) {
+                Log.d("test", "Error - " + t.getMessage());
+            }
+        });
+    }
 }
