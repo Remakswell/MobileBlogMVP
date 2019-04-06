6d3326580c67cbb8c48ef398c5fd3aac6334141b
maksym <maksymshymko@gmail.com>
2019-04-06 19:09:24 +0300

    ButterKnife implementation

--- app/src/main/java/com/example/mobileblogmvp/Main/FindItemsInteractor.java
+++ app/src/main/java/com/example/mobileblogmvp/Main/FindItemsInteractor.java
@@ -1,7 +1,6 @@
 package com.example.mobileblogmvp.Main;
 import com.example.mobileblogmvp.ApiClient;
 import com.example.mobileblogmvp.ApiInterface;
-import com.example.mobileblogmvp.Models.AuthorizationResponse;
 import com.example.mobileblogmvp.Models.ProjectsResponse;
 import java.util.List;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -13,7 +12,6 @@ public class FindItemsInteractor {
 
     private MainView mainView;
     private final ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
-    private AuthorizationResponse authorizationResponse = new AuthorizationResponse();
 
     public FindItemsInteractor(MainView mainView){
         this.mainView = mainView;
